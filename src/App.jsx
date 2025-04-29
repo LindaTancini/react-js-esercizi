f0578bc713f3ca874fddc354e30fd7761e6514f5
LindaTancini <lindatancini95@gmail.com>
2025-04-29 10:01:58 +0200

    feat: add second task

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 //IMPORTAZIONI
+import EsercizioDue from "./components/EsercizioDue";
 import EsercizioUno from "./components/EsercizioUno";
 
 function App() {
   return (
     <>
       <EsercizioUno />
+      <EsercizioDue />
     </>
   );
 }
---
+++ src/components/EsercizioDue.jsx
@@ -0,0 +1,33 @@
+//IMPORTAZIONI
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function EsercizioDue() {
+  //CREO STATI
+  const [users, setUsers] = useState([]);
+  //CHIAMATA AXIOS
+  useEffect(() => {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    axios
+      .get(url)
+      .then((response) => setUsers(response.data))
+      .catch((error) =>
+        console.error("Errore durante la chiamata API:", error)
+      );
+  }, []);
+  return (
+    <div>
+      <h2>Lista utenti</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+//ESPORTO
+export default EsercizioDue;
